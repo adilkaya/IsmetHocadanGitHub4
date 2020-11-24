794f2b53f371e7b2a30f50298f4f2e63041a058b
Adil Kaya <@adil.kayaa2@gmail.com>
2020-11-24 20:33:33 +0100

    Sepete ekle kismi yapildi

--- src/Test4.java
+++ src/Test4.java
@@ -2,4 +2,9 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.println("1.islem");
     }
+    public static void yeniMetod()
+    {
+        System.out.println("Yeni metodum hazir");
+        System.out.println("Sepete ekle kisimlari buralarmis");
+    }
 }
